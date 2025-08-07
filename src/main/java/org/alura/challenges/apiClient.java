69664282605dd703f025ae399d146f27b854b23e
LazaroTupo <danielalorenzoramos@gmail.com>
2025-08-07 05:47:29 -0500

    add env

--- src/main/java/org/alura/challenges/apiClient.java
+++ src/main/java/org/alura/challenges/apiClient.java
@@ -1,17 +1,20 @@
 package org.alura.challenges;
 
 import com.google.gson.Gson;
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
 class ApiClient {
-    private static final String API_BASE_URL = "https://v6.exchangerate-api.com/v6/c89e55965dc652e0c853bc0e/latest/";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String API_KEY = dotenv.get("API_KEY");
+    private static final String API_BASE_URL = "https://v6.exchangerate-api.com/v6/";
 
     public static double convertCurrency(String fromCurrency, String toCurrency, double value) throws IOException {
         // 1. Construir URL dinámica con fromCurrency
-        String apiUrl = API_BASE_URL + fromCurrency;
+        String apiUrl = API_BASE_URL + API_KEY + "/latest/" + fromCurrency;
         URL url = new URL(apiUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
